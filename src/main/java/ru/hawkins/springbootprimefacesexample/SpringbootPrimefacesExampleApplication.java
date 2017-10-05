a356cd5949e2ce1ffa591ed3a5b85c1af1f9f849
Egor Yastrebov <astore.hawk@gmail.com>
2017-10-05 13:39:12 +0300

    Configured Faces with Spring Boot

--- src/main/java/ru/hawkins/springbootprimefacesexample/SpringbootPrimefacesExampleApplication.java
+++ src/main/java/ru/hawkins/springbootprimefacesexample/SpringbootPrimefacesExampleApplication.java
@@ -2,11 +2,36 @@ package ru.hawkins.springbootprimefacesexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.ServletContextAware;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
 
 @SpringBootApplication
-public class SpringbootPrimefacesExampleApplication {
+public class SpringbootPrimefacesExampleApplication implements ServletContextAware {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootPrimefacesExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPrimefacesExampleApplication.class, args);
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+        servletContext.setInitParameter("primefaces.CLIENT_SIDE_VALIDATION",Boolean.TRUE.toString());
+    }
+
+    @Bean
+    public FacesServlet facesServlet() {
+        return new FacesServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean facesServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                facesServlet(), "*.xhtml");
+        registration.setName("FacesServlet");
+        return registration;
+    }
 }
