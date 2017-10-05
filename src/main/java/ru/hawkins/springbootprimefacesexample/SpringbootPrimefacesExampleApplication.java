7751b198d29c42b8b1d90a2fffd4cf11d27aafee
Egor Yastrebov <astore.hawk@gmail.com>
2017-10-05 13:07:49 +0300

    Spring Initializr

---
+++ src/main/java/ru/hawkins/springbootprimefacesexample/SpringbootPrimefacesExampleApplication.java
@@ -0,0 +1,12 @@
+package ru.hawkins.springbootprimefacesexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPrimefacesExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPrimefacesExampleApplication.class, args);
+	}
+}
