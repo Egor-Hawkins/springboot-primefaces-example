4620fcb738cf23780a429fd6f0c5549c9508c44f
Egor Yastrebov <astore.hawk@gmail.com>
2017-10-05 18:57:23 +0300

    #4 Added log4j2 support

--- src/main/java/ru/hawkins/springbootprimefacesexample/views/TestController.java
+++ src/main/java/ru/hawkins/springbootprimefacesexample/views/TestController.java
@@ -1,6 +1,8 @@
 package ru.hawkins.springbootprimefacesexample.views;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +13,11 @@ import javax.annotation.PostConstruct;
 public class TestController {
     private String someString;
 
+    private static final Logger logger = LogManager.getLogger(TestController.class);
+
     @PostConstruct
     public void init() {
+        logger.info("Test view created");
         someString  = "some string value from bean";
     }
 
