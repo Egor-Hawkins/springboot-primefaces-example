c7baf0e18b9099df1526ca30c660a01ac2776e3e
Egor Yastrebov <astore.hawk@gmail.com>
2017-11-20 17:20:55 +0300

    Added faces view scope

--- src/main/java/ru/hawkins/springbootprimefacesexample/SpringbootPrimefacesExampleApplication.java
+++ src/main/java/ru/hawkins/springbootprimefacesexample/SpringbootPrimefacesExampleApplication.java
@@ -1,13 +1,17 @@
 package ru.hawkins.springbootprimefacesexample;
 
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.ServletContextAware;
+import ru.hawkins.springbootprimefacesexample.jsf.ViewScope;
 
 import javax.faces.webapp.FacesServlet;
 import javax.servlet.ServletContext;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class SpringbootPrimefacesExampleApplication implements ServletContextAware {
@@ -33,5 +37,14 @@ public class SpringbootPrimefacesExampleApplication implements ServletContextAwa
                 facesServlet(), "*.xhtml");
         registration.setName("FacesServlet");
         return registration;
+    }
+
+    @Bean
+    public static CustomScopeConfigurer scopeConfigurer() {
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        Map<String, Object> scopesMap = new HashMap<>();
+        scopesMap.put("view", new ViewScope());
+        configurer.setScopes(scopesMap);
+        return configurer;
     }
 }
